4169c297628bc2c12410e61447e06867b74832d5
timothygeurts <160869250+timothygeurts@users.noreply.github.com>
2026-07-01 13:00:17 +0200

    Fix conductor

---
+++ Conducter_Enemy.gd
@@ -0,0 +1,5 @@
+extends CharacterBody3D
+
+var pla
+
+const SPEED = 5.0
--- Scripts/3DenemyAI.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-@onready var target=$Player
-
-
-func _process(delta):
-	get_tree().call_group("enemy" , "target_position" , target.global_transform.origin)
